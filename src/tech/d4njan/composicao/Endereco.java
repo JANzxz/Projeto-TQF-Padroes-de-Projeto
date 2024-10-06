ab151bb4201c16d5a5bd7f28dc7d03558db770b7
JANzxz <joaovitorexz@gmail.com>
2024-10-05 21:39:07 -0300

    herança de funcionario e correções

--- src/tech/d4njan/composicao/Endereco.java
+++ src/tech/d4njan/composicao/Endereco.java
@@ -1,5 +1,38 @@
 package tech.d4njan.composicao;
 
-public class Endereco {
+public class Endereco{
 
+    private String rua;
+    private String cidade;
+    private String estado;
+
+    public Endereco(String rua, String cidade, String estado){
+        this.rua = rua;
+        this.cidade = cidade;
+        this.estado = estado;
+    }
+
+    //Get set de rua
+    public String getRua(){
+        return rua;
+    }
+    public void setRua (String rua) {
+        this.rua = rua;
+    }
+
+    //Get set de cidade
+    public String getCidade (){
+        return cidade;
+    }
+    public void setCidade (String cidade){
+        this.cidade = cidade;
+    }
+
+    //Get set de estado
+    public String getEstado (){
+        return estado;
+    }
+    public void setEstado (String estado){
+        this.estado = estado;
+    }
 }
--- src/tech/d4njan/pessoas/Funcionario.java
+++ src/tech/d4njan/pessoas/Funcionario.java
@@ -1,5 +1,5 @@
 package tech.d4njan.pessoas;
 
-public class Funcionario {
+public class Funcionario extends Pessoa {
 
 }
